c4541343ba9591d46600b7664d841c3d0050eac9
liewchooichin <liewchooichin@gmail.com>
2024-08-17 15:30:13 +0800

    async promise

--- out/num12.js
+++ out/num12.js
@@ -1,8 +1,6 @@
 "use strict";
-
 let fifteen = Promise.resolve(15);
 fifteen.then(value => console.log(`Got ${value}`));
-
 const vege = {
     "favo": {
         "favo1": "brocolli",
@@ -12,31 +10,15 @@ const vege = {
     "avoid": {
         "avoid1": "carrot",
     }
-}
-
+};
 const vege_prom = Promise.resolve(JSON.stringify(vege));
 vege_prom.then(value => console.log(vege_prom));
-
 console.log(vege["avoid"]);
-
-
-const another15 = new Promise(
-    (_, reject) => reject("My failed async 345")
-)
+const another15 = new Promise((_, reject) => reject("My failed async 345"));
 another15.then(value => console.log(value))
     .catch(reason => console.log(reason));
-
-
-const yetAnother15 = new Promise(
-    (   (resolve, reject) => {
-            resolve("My resolved async 198"), 
-            reject("My failed async 198")
-        }
-    )
-)
-
-yetAnother15.then(
-    (value) => {console.log(value);}
-).catch(
-    (reason) => {console.log(reason);}
-)
+const yetAnother15 = new Promise(((resolve, reject) => {
+    resolve("My resolved async 198"),
+        reject("My failed async 198");
+}));
+yetAnother15.then((value) => { console.log(value); }).catch((reason) => { console.log(reason); });
